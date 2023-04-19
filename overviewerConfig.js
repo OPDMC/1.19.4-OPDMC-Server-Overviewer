f5c1e65e63feef26f0aa062d9c5579bc46007e31
SynthesisDu <75297777+TitanRGB@users.noreply.github.com>
2023-04-19 08:18:59 -0400

    4.19

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1681736605",
+        "cacheTag": "1681906663",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world - overworld",
-            "last_rendertime": 1681735366,
+            "last_rendertime": 1681906113,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
