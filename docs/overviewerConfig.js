599f47957f3d9975a551cb672da0c4978279bd30
TitanRGB <75297777+TitanRGB@users.noreply.github.com>
2023-04-25 04:06:42 -0400

    23-04-25

---
+++ docs/overviewerConfig.js
@@ -0,0 +1,72 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "world - overworld"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1682389388",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Day",
+            "zoomLevels": 9,
+            "defaultZoom": 5,
+            "maxZoom": 9,
+            "path": "day",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world - overworld",
+            "last_rendertime": 1682388110,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                16,
+                144,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                16,
+                144,
+                0
+            ],
+            "north_direction": 0
+        }
+    ]
+};
