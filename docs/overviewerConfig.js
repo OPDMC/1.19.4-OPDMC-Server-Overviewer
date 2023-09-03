aee9bf33ae6dbafd78a2cdf1525bab425e670a66
TitanRGB <75297777+Mehver@users.noreply.github.com>
2023-09-03 12:40:17 -0400

    23-09-03

--- docs/overviewerConfig.js
+++ docs/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1683592683",
+        "cacheTag": "1693717607",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world - overworld",
-            "last_rendertime": 1683591054,
+            "last_rendertime": 1693716084,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
